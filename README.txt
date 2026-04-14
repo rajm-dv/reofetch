e181395419c7e307d004d7755133469f6b368345
rajm-dv <dev.rajm8923@gmail.com>
2026-04-14 15:39:51 +0530

    init

---
+++ README.txt
@@ -0,0 +1,17 @@
+# rfetch
+
+## Add-ons
+
+### Left side
+- OS Logo
+
+### Right side
+- user@host
+- OS info
+- Kernel info
+- Uptime info
+- Shell info
+- CPU info
+- GPU info
+- Memory info
+
---
+++ rfetch.c
@@ -0,0 +1,17 @@
+/*
+ * <DES>
+ * Github.com/rajm-dv/rfetch
+ * rfetch -> neofetch clone
+ * Auther: rajm-dv
+ * Date: 14-04-2026
+ * </DES>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  printf("Hello world\n");
+
+  return 0;
+}
