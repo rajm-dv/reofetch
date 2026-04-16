fc36d234426dcb0bed4fae05053787b1ce2295b2
rajm-dv <dev.rajm8923@gmail.com>
2026-04-16 15:49:41 +0530

    updated get_os.c

--- get_os.c
+++ get_os.c
@@ -5,9 +5,12 @@ void get_os(void) {
   FILE *fp = fopen("/etc/os-release", "r");
   char line[256];
 
-  while(fgets(line, sizeof(line), fp)) {
-    if(strncmp(line, "PRETTY_NAME=", 12) == 0) {
-      printf("OS: %s", line + 12);
+  while (fgets(line, sizeof(line), fp)) {
+    if (strncmp(line, "PRETTY_NAME=", 12) == 0) {
+      char *ptr = strrchr(line, '"');
+      int rpos = (int)(ptr - line);
+      line[rpos] = '\0';
+      printf("OS: %s\n", line + 13);
       break;
     }
   }
